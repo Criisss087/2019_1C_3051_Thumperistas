6d496fb65027523fee3693aaaa72b3c6d3f79ac4
Criisss087 <cristian.barrientos@hotmail.com>
2019-06-10 03:31:27 -0300

    Start menu WIP

---
+++ TGC.Group/Model/StartMenu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TGC.Core.Terrain;
+
+namespace TGC.Group.Model
+{
+    private TgcSkyBox fondo;
+
+    class StartMenu
+    {
+
+
+    }
+}
